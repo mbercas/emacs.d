19d03386f040cfd8dec1bc29af6db1786e41b315
mbercas <mbercas@gmail.com>
2019-11-09 18:26:59 +0100

    Add switches to configuration that is OS dependent

--- mygtg.el
+++ mygtg.el
@@ -1,7 +1,19 @@
+(cond
+ ((string-equal system-type "windows-nt") ; Microsoft Windows
+   (setq org-default-notes-file (concat org-directory "C:/Users/manuel/Dropbox/Apps/org/notes.org"))
+   (setq org-capture-templates
+      '(("t" "Todo" entry (file+headline "C:/Users/manuel/Dropbox/Apps/org/gtd.org" "Tasks")
+         "* TODO %?\n  %i\n  %a")
+        ("n" "Note" entry (file "C:/Users/manuel/Dropbox/Apps/org/notes.org")
+         "* NOTE %?\n  %i\n %a")
+        ("p" "Proj" entry (file "C:/Users/manuel/Dropbox/Apps/org/projects.org")
+         "** TODO %?\n  %i\n %a")
+        ("j" "Journal" entry (file+olp+datetree "C:/Users/manuel/Dropbox/Apps/org/journal.org")
+         "* %?\nEntered on %U\n  %i\n  %a"))))
+ ((string-equal system-type "gnu/linux") ; linux
+    (setq org-default-notes-file (concat org-directory "~/org/notes.org"))
 
-(setq org-default-notes-file (concat org-directory "~/org/notes.org"))
-
-(setq org-capture-templates
+    (setq org-capture-templates
       '(("t" "Todo" entry (file+headline "~/org/gtd.org" "Tasks")
          "* TODO %?\n  %i\n  %a")
         ("n" "Note" entry (file "~/org/notes.org")
@@ -9,7 +21,7 @@
         ("p" "Proj" entry (file "~/org/projects.org")
          "** TODO %?\n  %i\n %a")
         ("j" "Journal" entry (file+olp+datetree "~/org/journal.org")
-         "* %?\nEntered on %U\n  %i\n  %a")))
+         "* %?\nEntered on %U\n  %i\n  %a")))))
 
 ; Refile on top of file max
 (setq org-refile-use-outline-path 'file)
@@ -18,75 +30,7 @@
       '((org-agenda-files . (:maxlevel . 2))))
 (setq org-outline-path-complete-in-steps nil)
 
-(setq org-mobile-directory "/sandbox/Dropbox/MobileOrg")
-
 (setq org-log-done 'time)
-
-(defcustom org-mactions-numbered-action-format "TODO Action #%d "
-  "Default structure of the headling of a new action.
-    %d will become the number of the action."
-  :group 'org-edit-structure
-  :type 'string)
-
-(defcustom org-mactions-change-id-on-copy t
-  "Non-nil means make new IDs in copied actions.
-If an action copied with the command `org-mactions-collect-todos-in-subtree'
-contains an ID, that ID will be replaced with a new one."
-  :group 'org-edit-structure
-  :type 'string)
-
-
-(defun org-mactions-new-numbered-action (&optional inline)
-  "Insert a new numbered action, using `org-mactions-numbered-action-format'.
-    With prefix argument, insert an inline task."
-  (interactive "P")
-  (let* ((num (let ((re "\\`#\\([0-9]+\\)\\'"))
-                (1+ (apply 'max 0
-                           (mapcar
-                            (lambda (e)
-                              (if (string-match re (car e))
-                                  (string-to-number (match-string 1 (car e)))
-                                0))
-                            (org-get-buffer-tags))))))
-         (tag (concat "#" (number-to-string num))))
-    (if inline
-        (org-inlinetask-insert-task)
-      (org-insert-heading 'force))
-    (unless (eql (char-before) ?\ ) (insert " "))
-    (insert (format org-mactions-numbered-action-format num))
-    (org-toggle-tag tag 'on)
-    (if (= (point-max) (point-at-bol))
-        (save-excursion (goto-char (point-at-eol)) (insert "\n")))
-    (unless (eql (char-before) ?\ ) (insert " "))))
-
-(defun org-mactions-collect-todos-in-subtree ()
-  "Collect all TODO items in the current subtree into a flat list."
-  (interactive)
-  (let ((buf (get-buffer-create "Org TODO Collect"))
-        (cnt 0) beg end string s)
-    (with-current-buffer buf (erase-buffer) (org-mode))
-    (org-map-entries
-     (lambda ()
-       (setq beg (point) end (org-end-of-subtree t t) cnt (1+ cnt)
-             string (buffer-substring beg end)
-             s 0)
-       (when org-mactions-change-id-on-copy
-         (while (string-match "^\\([ \t]*:ID:\\)[ \t\n]+\\([^ \t\n]+\\)[ \t]*$"
-                              string s)
-           (setq s (match-end 1)
-                 string (replace-match (concat "\\1 "
-                                               (save-match-data (org-id-new)))
-                                       t nil string))))
-       (with-current-buffer buf (org-paste-subtree 1 string)
-                            (goto-char (point-max))))
-     (format "TODO={%s}" (regexp-opt org-not-done-keywords))
-     'tree)
-    (if (= cnt 0)
-        (message "No TODO items in subtree")
-      (message "%d TODO entries copied to kill ring" cnt)
-      (prog1 (with-current-buffer buf
-               (kill-new (buffer-string)))
-        (kill-buffer buf)))))
 
 (setq org-agenda-overriding-columns-format "%58ITEM(Details) %PRIORITY(P) %TAGS(Context) %7TODO(To Do) %5Effort(Time){:} %6CLOCKSUM{Total}")
 
@@ -155,21 +99,21 @@ Callers of this function already widen the buffer view."
 ;; Auto-update tags whenever the state is changed
 (setq org-todo-state-tags-triggers
       '(("CANCELLED" ("CANCELLED" . t))
-        ("WAITING" ("WAITING" . t))
-        ("INACTIVE" ("WAITING") ("INACTIVE" . t))
-        (done ("WAITING") ("INACTIVE"))
-        ("TODO" ("WAITING") ("CANCELLED") ("INACTIVE"))
-        ("NEXT" ("WAITING") ("CANCELLED") ("INACTIVE"))
-        ("DONE" ("WAITING") ("CANCELLED") ("INACTIVE"))))
+	("WAITING" ("WAITING" . t))
+	("INACTIVE" ("WAITING") ("INACTIVE" . t))
+	(done ("WAITING") ("INACTIVE"))
+	("TODO" ("WAITING") ("CANCELLED") ("INACTIVE"))
+	("NEXT" ("WAITING") ("CANCELLED") ("INACTIVE"))
+	("DONE" ("WAITING") ("CANCELLED") ("INACTIVE"))))
 
 (defun gs/mark-next-done-parent-tasks-todo ()
   "Visit each parent task and change NEXT (or DONE) states to TODO."
   ;; Don't change the value if new state is "DONE"
   (let ((mystate (or (and (fboundp 'org-state)
                           (member state
-                                  (list "NEXT" "TODO")))
+				  (list "NEXT" "TODO")))
                      (member (nth 2 (org-heading-components))
-                             (list "NEXT" "TODO")))))
+			     (list "NEXT" "TODO")))))
     (when mystate
       (save-excursion
         (while (org-up-heading-safe)
